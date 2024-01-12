964125b7494107fff89cffba14f5fa662b021352
Daniela Román <90222511+danielaro2@users.noreply.github.com>
2024-01-11 19:11:39 -0500

    hasta recycler view implementado en actividad

--- 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/FRecyclerView.kt
+++ 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/FRecyclerView.kt
@@ -2,10 +2,27 @@ package com.example.gr1accdlrc2023b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class FRecyclerView : AppCompatActivity() {
+    var totalLikes = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frecycler_view)
+        inicializarRecyclerView()
+    }
+    fun inicializarRecyclerView(){
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_entrenadores)
+        val adaptador = FRecyclerViewAdaptadorNombreDescripcion(
+            this, // Contexto
+            BBaseDatosMemoria.arregloBEntrenador, // Arreglo datos
+            recyclerView // Recycler view
+        )
+        recyclerView.adapter = adaptador
+        recyclerView.itemAnimator = androidx.recyclerview.widget
+            .DefaultItemAnimator()
+        recyclerView.layoutManager = androidx.recyclerview.widget
+            .LinearLayoutManager(this)
+        adaptador.notifyDataSetChanged()
     }
 }
