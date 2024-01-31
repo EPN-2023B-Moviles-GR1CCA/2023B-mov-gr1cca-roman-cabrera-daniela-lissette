d3d7d126261592387f02658bc0c377f46ebda0e5
Daniela Román <90222511+danielaro2@users.noreply.github.com>
2024-01-31 17:27:58 -0500

    Hasta list view

---
+++ 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/ICities.kt
@@ -0,0 +1,15 @@
+package com.example.gr1accdlrc2023b
+
+class ICities (
+    public var name: String?,
+    public var state: String,
+    public var country: String?,
+    public var capital: Boolean?,
+    public var population: Long?,
+    public var regions: List<String>?
+) {
+    override fun toString(): String{
+        return "${name} - ${country}"
+    }
+
+}
--- 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/IFirestore.kt
+++ 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/IFirestore.kt
@@ -2,10 +2,35 @@ package com.example.gr1accdlrc2023b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class IFirestore : AppCompatActivity() {
+
+    var query: Query? = null
+    val arreglo: ArrayList<ICities> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ifirestore)
+
+        //Configurando el list view
+        val listView = findViewById<ListView>(R.id.lv_firestore)
+        val adaptador = ArrayAdapter(
+            this,
+            android.R.layout.simple_liste_item_1,
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
     }
 }
