138c0ee7a64a13615e52c64be61beee6d57c0e26
Daniela Román <90222511+danielaro2@users.noreply.github.com>
2024-02-01 18:57:31 -0500

    Hasta consultar

--- 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/ICities.kt
+++ 02-andriod/app/src/main/java/com/example/gr1accdlrc2023b/ICities.kt
@@ -1,15 +1,14 @@
 package com.example.gr1accdlrc2023b
 
-class ICities (
+class ICities(
     public var name: String?,
-    public var state: String,
+    public var state: String?,
     public var country: String?,
     public var capital: Boolean?,
     public var population: Long?,
     public var regions: List<String>?
 ) {
-    override fun toString(): String{
+    override fun toString(): String {
         return "${name} - ${country}"
     }
-
 }
